f6d39501451537fa60b23f2cc069894d65d47ebd
w3tsa <49335574+w3tsa@users.noreply.github.com>
2020-06-24 15:12:35 -0700

    Update index.js

--- Image Search with API Request/index.js
+++ Image Search with API Request/index.js
@@ -2,36 +2,45 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const loadingImage = document.querySelector("#loadingImage");
 const imageSection = document.querySelector(".images");
+const loader = document.querySelector(".loader");
 
 loadingImage.style.display = "none";
-
+loader.style.display = "none";
 form.addEventListener("submit", formSubmited);
 
 function formSubmited(event) {
   event.preventDefault();
   const searchTerm = input.value;
-
-  search(searchTerm).then(displayImages);
+  let pageNumber = 1;
+  search(searchTerm, pageNumber).then(displayImages);
+  loader.addEventListener("click", () => {
+    pageNumber++;
+    search(searchTerm, pageNumber).then(displayImages);
+  });
 }
 
-function search(searchTerm) {
+function search(searchTerm, pageNumber) {
   loadingImage.style.display = "";
   imageSection.innerHTML = "";
+  loader.style.display = "";
   return fetch(
-    `https://pixabay.com/api/?key=16140890-cfd2c55f29001c6868f1a986d&q=${searchTerm}&image_type=photo`
+    `https://pixabay.com/api/?key=16140890-cfd2c55f29001c6868f1a986d&per_page=20&page=${pageNumber}&q=${searchTerm}&image_type=photo`
   )
     .then((Response) => Response.json())
     .then((result) => {
+      console.log(result);
       return result.hits;
     });
 }
 
 function displayImages(images) {
   images.forEach((image) => {
-    console.log(image.fullHDURL);
     const imageElement = document.createElement("img");
+    const url = document.createElement("a");
+    url.href = image.imageURL;
     imageElement.src = image.fullHDURL;
-    imageSection.appendChild(imageElement);
+    url.appendChild(imageElement);
+    imageSection.appendChild(url);
   });
   loadingImage.style.display = "none";
 }

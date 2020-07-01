17e5d94ce357376d65cc13600963ce01418ddf3c
w3tsa <49335574+w3tsa@users.noreply.github.com>
2020-06-30 20:28:30 -0700

    Update app.js

--- Todo with Local storage/app.js
+++ Todo with Local storage/app.js
@@ -5,6 +5,7 @@ const markAllButton = document.querySelector(".marked");
 const deleteButton = document.querySelector(".delete-all");
 
 //Event Listeners
+
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
@@ -18,32 +19,33 @@ newTodo.addEventListener("keyup", function (e) {
 //Functions
 function addTodo(event) {
   event.preventDefault();
-  //Todo Div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todos");
-  //Creat Li
-  const todo = document.createElement("li");
-  todo.innerText = newTodo.value;
-  todo.classList.add("list-group-item");
-  todoDiv.appendChild(todo);
-  //Add todo in local storage
-  saveLocalTodos(newTodo.value);
-  //Checked button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add("check-btn", "btn", "btn-outline-secondary");
-  todoDiv.appendChild(completedButton);
-  //Trash Button
-  const trashedButton = document.createElement("button");
-  trashedButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashedButton.classList.add("btn", "btn-outline-secondary", "trash-btn");
-  todoDiv.appendChild(trashedButton);
-  //Append to list
-  todoList.appendChild(todoDiv);
-  //Clear the input value
-  newTodo.value = "";
+  if (newTodo.value.length > 0) {
+    //Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todos");
+    //Creat Li
+    const todo = document.createElement("li");
+    todo.innerText = newTodo.value;
+    todo.classList.add("list-group-item");
+    todoDiv.appendChild(todo);
+    //Add todo in local storage
+    saveLocalTodos(newTodo.value);
+    //Checked button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("check-btn", "btn", "btn-outline-secondary");
+    todoDiv.appendChild(completedButton);
+    //Trash Button
+    const trashedButton = document.createElement("button");
+    trashedButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashedButton.classList.add("btn", "btn-outline-secondary", "trash-btn");
+    todoDiv.appendChild(trashedButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear the input value
+    newTodo.value = "";
+  }
 }
-
 function deleteCheck(e) {
   const item = e.target;
   //Delete Todo
@@ -140,3 +142,52 @@ function removeDeleteAll(todoList) {
     /*todos = JSON.parse(localStorage.getItem("todos"));*/
   }
 }
+
+const Input = document.querySelector("#result");
+
+let InputValue = Input.value;
+
+document.addEventListener("DOMContentLoaded", get);
+Input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    saveLocal();
+  }
+});
+
+function saveLocal() {
+  let InputValue = Input.value;
+  if (typeof Storage !== "undefined") {
+    // Store
+    localStorage.setItem("title", InputValue);
+    // Retrieve
+    document.querySelector("#result").value = localStorage.getItem("title");
+  }
+}
+
+function get() {
+  if (typeof Storage !== "undefined") {
+    document.querySelector("#result").value = localStorage.getItem("title");
+  }
+}
+
+function setBgImg() {
+  let body = document.querySelector("body");
+  let today = new Date(),
+    hour = today.getHours();
+  console.log(hour);
+  if (hour < 12) {
+    body.style.background =
+      "url('https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80') no-repeat";
+    body.style.backgroundSize = "cover";
+  } else if (hour < 18) {
+    body.style.background =
+      "url('https://images.unsplash.com/photo-1494216928456-ae75fd96603d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80') no-repeat";
+    body.style.backgroundSize = "cover";
+  } else {
+    body.style.background =
+      "url('https://images.unsplash.com/photo-1511884642898-4c92249e20b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80) no-repeat";
+    body.style.backgroundSize = "cover";
+  }
+}
+
+setBgImg();
